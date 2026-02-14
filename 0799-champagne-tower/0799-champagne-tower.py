19766bf37451607e784a836ebfa658c2c91e9f99
Jayesh404 <163423517+Jayesh404@users.noreply.github.com>
2026-02-14 17:57:59 +0530

    Time: 64 ms (65.52%), Space: 12.6 MB (58.62%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def champagneTower(self, poured, query_row, query_glass):
-        """
-        :type poured: int
-        :type query_row: int
-        :type query_glass: int
-        :rtype: float
-        """
-        
+        A = [[0] * k for k in xrange(1, 102)]
+        A[0][0] = poured
+        for r in xrange(query_row + 1):
+            for c in xrange(r+1):
+                q = (A[r][c] - 1.0) / 2.0
+                if q > 0:
+                    A[r+1][c] += q
+                    A[r+1][c+1] += q
+
+        return min(1, A[query_row][query_glass])
